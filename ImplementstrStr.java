ad963fd74269aa9f1c686d9f80d79066124c3fa2
Jinglin-LI <lijinglin@mail.nankai.edu.cn>
2016-01-06 10:58:34 +0800

    Update ImplementstrStr.java
    Didn't notice that API cannot be used in the interview.

--- ImplementstrStr.java
+++ ImplementstrStr.java
@@ -5,11 +5,22 @@ Returns the index of the first occurrence of needle in haystack, or -1 if needle
 
 public class Solution {
     public int strStr(String haystack, String needle) {
-    int index = haystack.indexOf(needle);  
-        if(index < 0)      
-            return -1;  
-        else            
-            return index;
+    
+    if (needle.length() == 0)
+        return 0;
+    if (needle.length() > haystack.length())
+        return -1;
+    int i, j;
+    for (j = 0; j < haystack.length(); j++) {
+            for (i = 0; i < needle.length() && i + j < haystack.length() && needle.charAt(i) == haystack.charAt(i + j); i++)
+            ;
+            if (i == needle.length())
+                return j;
+            if (i + j == haystack.length())
+                return -1;
     }
+    	return -1;
+    }
+    
 }
     
