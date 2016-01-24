3b4a8e5d69ef0196ed6a7c7422cf17ddb000ddd3
Jinglin-LI <lijinglin@mail.nankai.edu.cn>
2016-01-24 13:40:15 +0800

    Create ReverseBits.java

---
+++ ReverseBits.java
@@ -0,0 +1,17 @@
+/*
+Reverse bits of a given 32 bits unsigned integer.
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+*/
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = n & (1 << i);
+            if (bit != 0)
+                res = res + (1 << (31 - i));
+        }
+        return res;
+    }
+}
