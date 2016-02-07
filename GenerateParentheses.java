292581cbf2d1cb9a2758e3f9c1fcc41a3eb65f3e
Jinglin-LI <lijinglin@mail.nankai.edu.cn>
2016-02-07 11:57:23 +0800

    Backtracking method

---
+++ GenerateParentheses.java
@@ -0,0 +1,25 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+For example, given n = 3, a solution set is:
+"((()))", "(()())", "(())()", "()(())", "()()()"
+*/
+
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        if (n <= 0)
+            return res;
+        String str = "";
+        helper(res, n, 1, 0, "(");
+        return res;
+    }
+    private void helper(List<String> res, int n, int leftN, int rightN, String str) {
+        if (leftN == n && rightN == n)
+            res.add(str);
+            
+        if (leftN < n)
+            helper(res, n, leftN + 1, rightN, str + "(");
+        if (leftN > rightN)
+            helper(res, n, leftN, rightN + 1, str + ")");
+    }
+}
