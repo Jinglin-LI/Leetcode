b519693c8c1730dd55dd085f74d9ffd3a8c1d548
Jinglin-LI <lijinglin@mail.nankai.edu.cn>
2016-03-15 23:10:25 +0800

    Locked

---
+++ ClosestBinarySearchTreeValue.java
@@ -0,0 +1,39 @@
+/*
+Total Accepted: 1815 Total Submissions: 6418 Difficulty: Easy
+Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+Note:
+Given target value is a floating point.
+You are guaranteed to have only one unique value in the BST that is closest to the target.
+*/
+// 最近的node是：如果target < root.val, 是左子数或者根节点，如果target > root.val, 是右子树或根节点。
+
+/** 
+ * Definition for a binary tree node. 
+ * public class TreeNode { 
+ *     int val; 
+ *     TreeNode left; 
+ *     TreeNode right; 
+ *     TreeNode(int x) { val = x; } 
+ * } 
+ */ 
+ 
+public class Solution {
+	public int closestValue(TreeNode root, double target) {
+		int closest = root.val;
+		double min = Double.MAX_VALUE;
+		
+		while (root != null) {
+			if (Math.abs(root.val - target) < min) {
+				min = Math.abs(root.val - target);
+				closest = root.val;                           // the closest node is the root.val
+			}
+			if (target < root.val)
+				root = root.left;                             // if target < root.val, check which is the closest node, root.left or root?
+			else if (target > root.val)
+				root = root.right;
+			else
+				return root.val;
+		}
+		return closest;
+	}
+}
