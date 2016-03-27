82266d564c8655b6a4c6306c24790b29809c321a
Jinglin-LI <lijinglin@mail.nankai.edu.cn>
2016-03-27 16:23:25 +0800

    Hard, hard, hard。BFS

---
+++ WordLadder.java
@@ -0,0 +1,69 @@
+/*
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time
+Each intermediate word must exist in the word list
+For example,
+Given:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+
+Note:
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+*/
+/*
+解题，思路可见http://blog.sina.com.cn/s/blog_eb52001d0102v2ds.html。
+此题是undirected graph（两个只差一个字符的单词连在一起,如下）. 找最短的两个节点的距离。
+                    hit - hot
+                         /   \
+                       dot - lot
+                        |     |
+                       dog - log - cog
+hit建立的word[]为ait,bit,cit..., hat,hbt, hct..., hia,hib,hic...
+检测word[j]是否有endWord,是则返回res + 1; 检测其是否存在于wordList,是则将其存在q中。
+curCount和nextCount记录q中当前和下一个单词的candidate的数量，为了计数res（即q空了则从一个单词到下一个单词）。
+BFS比DFS效率高，比如111 - 311. 如果DFS, 111 - 112 - 112的所有变形; BFS则111 - 112 - 113 - 121 - 131 - 211 - 311.
+*/
+
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        if(beginWord == null || endWord == null || beginWord.length() == 0 || endWord.length() == 0)
+            return 0;
+        Queue<String> q = new LinkedList<String>();           // 存储candidate的单词
+        q.add(beginWord);
+        int res = 1;
+        int curCount = 1;                                     // 当前单词的数量
+        int nextCount = 0;                                    // 下一个单词的candidate的数量
+        
+        while (!q.isEmpty()) {
+            String candidate = q.poll();
+            curCount--;
+            for(int i = 0; i < candidate.length(); i++) {
+                char[] words = candidate.toCharArray();
+                for (char j = 'a'; j <= 'z'; j++) {           // nit的words[]为ait,bit,cit...
+                    words[i] = j;
+                    String temp = new String(words);
+                    
+                    if (temp.equals(endWord))
+                        return res + 1;                       // 只有当candidate的单词最后能到endWord时，才返回res
+                    if (wordList.contains(temp)) {
+                        q.add(temp);
+                        nextCount++;
+                        wordList.remove(temp);                // 可以从给的dictionary里面删除
+                    }
+                }
+            }
+            if (curCount == 0) {                              // 当前这个单词的数量为零时，向后移动
+                curCount = nextCount;                         // 当前candidate的单词的数量为之前的nextCount
+                nextCount = 0;                                // 下一个candidate的单词的数量变为0
+                res++;
+            }
+        } 
+        return 0;
+    }
+}
