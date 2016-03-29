83b66259f0c2647024e4b28257375ffb11f55277
Jinglin-LI <lijinglin@mail.nankai.edu.cn>
2016-03-29 21:29:30 +0800

    DP

---
+++ HouseRobber.java
@@ -0,0 +1,27 @@
+/*
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and 
+it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
+*/
+// DP. 存与dp[i - 2]相加大或dp[i - 1]本身大的数。
+
+public class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0)
+            return 0;
+        if (len == 1)
+            return nums[0];
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < len; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[len - 1];
+    }
+}
