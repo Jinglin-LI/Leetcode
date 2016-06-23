d6617093423b5756ca570d8fb0318518d51b1ec3
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-06-23 16:10:22 +0800

    Add new solutions

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -14,28 +14,63 @@ Output: 7 -> 0 -> 8
  * }
  */
  
-public class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode p1 = l1;
-        ListNode p2 = l2;
-        ListNode dummyHead = new ListNode(0);
-        ListNode cur = dummyHead;
-        int carry = 0;
-        
-        while (p1 != null || p2 != null) {
-            int v1 = (p1 != null)? p1.val : 0;
-            int v2 = (p2 != null)? p2.val : 0;
-            int digit = v1 + v2 + carry;
-            carry = digit / 10;
-            cur.next = new ListNode(digit % 10);
-            cur = cur.next;
-            if (p1 != null)
-                p1 = p1.next;
-            if (p2 != null)
-                p2 = p2.next;
-        }
-        if (carry == 1)
-            cur.next = new ListNode(1);
-        return dummyHead.next;
-    }
+import java.util.*;
+public class AddTwoNumbers {
+	public static void main(String[] args) {
+		
+	}
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+		}
+	}
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode p1 = l1;
+		ListNode p2 = l2;
+		ListNode dummyHead = new ListNode(0);
+		ListNode curr = dummyHead;
+		int carry = 0;
+		
+		while (p1 != null || p2 != null) {
+			int v1 = (p1 != null)? p1.val : 0;
+			int v2 = (p2 != null)? p2.val : 0;
+			int digit = v1 + v2 + carry;
+			carry = digit / 10;
+			curr.next = new ListNode(digit % 10);
+			curr = curr.next;
+			if (p1 != null)
+				p1 = p1.next;
+			if (p2 != null)
+				p2 = p2.next;
+		}
+		if (carry != 0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
+	}
+	
+	public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+		int carry = 0;
+		ListNode dummyHead = new ListNode(0);
+		ListNode p1 = l1, p2 = l2, curr = dummyHead;
+		while (p1 != null || p2 != null) {
+			if (p1 != null) {
+				carry += p1.val;
+				p1 = p1.next;
+			}
+			if (p2 != null) {
+				carry += p2.val;
+				p2 = p2.next;
+			}
+			int digit = carry % 10;
+			curr.next = new ListNode(digit);
+			curr = curr.next;
+			carry /= 10;
+		}
+		if (carry != 0)
+			curr.next = new ListNode(carry);
+		return dummyHead.next;
+	}
 }
