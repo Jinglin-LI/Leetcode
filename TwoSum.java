8b1d6d4fa5e4dc04e43558ab3bce22c7885d91e1
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-06-23 16:08:37 +0800

    Add new solutions

--- TwoSum.java
+++ TwoSum.java
@@ -6,19 +6,39 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
 
-public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] a = new int[2];
-        Map <Integer, Integer> map = new HashMap <> ();
-        for (int i = 0; i < nums.length; i++) {
-            int x = nums[i];
-            if (map.containsKey (target - x)) {
-                a[0] = map.get (target - x) + 1;
-                a[1] = i + 1;
-                return a;
-            }
-        map. put(x, i);
-        }
-        throw new IllegalArgumentException ("no such two numbers");
-    }
+public class TwoSum {
+	public static void main(String[] args) {
+		int[] numbers = {2, 7, 11, 15};
+		int target = 9;
+		int[] ans = new TwoSum().twoSum2(numbers, target);
+		System.out.print("[" + ans[0] + "," + ans[1] + "]");
+	}
+	// O(n2)
+	public int[] twoSum(int[] numbers, int target) {
+		int[] res = new int[2];
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				if (numbers[i] + numbers[j] == target) {
+					res[0] = i;
+					res[1] = j;
+					return res;
+				}
+			}
+		}
+		throw new IllegalArgumentException("No such two numbers");
+	}
+	// O(log n)
+	public int[] twoSum2(int[] numbers, int target) {
+		int[] res = new int[2];
+		Map <Integer, Integer> hm = new HashMap<>();
+		for (int i = 0; i < numbers.length; i++) {
+			if (hm.containsKey(target - numbers[i])) {
+				res[0] = hm.get(target - numbers[i]);
+				res[1] = i;
+				return res;
+			}
+			hm.put(numbers[i],i);
+		}
+		throw new IllegalArgumentException("No such two numbers by the twoSum2");
+	}
 }
