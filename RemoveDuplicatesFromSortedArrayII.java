1992bc9f7e2ab099db24c6a572b44d2fe5d20b9e
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-06-27 22:34:57 +0800

    Update RemoveDuplicatesFromSortedArrayII.java

--- RemoveDuplicatesFromSortedArrayII.java
+++ RemoveDuplicatesFromSortedArrayII.java
@@ -6,22 +6,24 @@ Given sorted array nums = [1,1,1,2,2,3],
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 */
 
-public class Solution {
-    public int removeDuplicates(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (!hm.containsKey(nums[i])) {
-                nums[count] = nums[i];
-                hm.put(nums[i], 1);
-                count++;
-            }
-            else if (hm.containsKey(nums[i]) && hm.get(nums[i]) == 1) {
-                nums[count] = nums[i];
-                hm.put(nums[i], 2);
-                count++;
-            }
-        }
-        return count;
-    }
+public class RemoveDuplicatesFromSortedArray {
+	public static void main(String[] args) {
+		int[] nums = {1,1,1,2,3};
+		System.out.println(new RemoveDuplicatesFromSortedArray().removeDuplicates(nums));
+	}
+	public int removeDuplicates(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
+		if (nums.length == 1)
+			return 1;
+		int count = 1;
+		
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[count - 1]) {
+				nums[count] = nums[i];
+				count++;
+			}
+		}
+		return count;
+	}
 }
