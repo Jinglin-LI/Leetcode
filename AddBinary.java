a62c387d5c00214513323ecd3875a335dc555ae5
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-06-28 17:24:37 +0800

    Update AddBinary.java

--- AddBinary.java
+++ AddBinary.java
@@ -7,34 +7,36 @@ b = "1"
 Return "100".
 */
 
-public class Solution {
-    public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int carry = 0, i = 0, j = 0;
-        if (a == null || b == null)
-            return a == null? b : a;
-        for (i = a.length() - 1, j = b.length() - 1; i >= 0 && j >= 0; i--, j--) {
-            int digit = (int)(a.charAt(i) - '0' + b.charAt(j) - '0') + carry;
-            carry = digit / 2;
-            digit = digit % 2;
-            sb.insert(0, digit);
-        }
-        while (i >= 0) {
-            int digit = (int) (a.charAt(i) - '0') + carry;
-            carry = digit / 2;
-            digit = digit % 2;
-            sb.insert(0, digit);
-            i--;
-        }
-        while (j >= 0) {
-            int digit = (int) (b.charAt(j) - '0') + carry;
-            carry = digit / 2;
-            digit = digit % 2;
-            sb.insert(0, digit);
-            j--;
-        }
-        if (carry > 0)
-            sb.insert(0, 1);
+public class AddBinary {
+	public static void main(String[] args) {
+		System.out.println(new AddBinary().addBinary("1", "111"));
+	}
+	public String addBinary(String a, String b) {
+		StringBuilder sb = new StringBuilder();
+		int i = a.length() - 1, j = b.length() - 1;
+		int carry = 0;
+		while (i >= 0 && j >= 0) {
+			carry += a.charAt(i) - '0' + b.charAt(j) - '0';
+			
+			sb.insert(0, carry % 2);
+			carry /= 2;
+			i--;
+			j--;
+		}
+		while (i >= 0) {
+			carry += a.charAt(i) - '0';
+			sb.insert(0, carry % 2);
+			carry /= 2;
+			i--;
+		}
+		while (j >= 0) {
+			carry += b.charAt(j) - '0';
+			sb.insert(0, carry % 2);
+			carry /= 2;
+			j--;
+		}
+		if (carry > 0)
+			sb.insert(0, 1);
 		return sb.toString();
-    }
+	}
 }
