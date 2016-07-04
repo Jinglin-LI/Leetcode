d07e1c83cdfa922e9164d63f673abe836fd349c3
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-07-04 19:47:13 +0800

    Update RangeSumQuery_Immutable.java

--- RangeSumQuery_Immutable.java
+++ RangeSumQuery_Immutable.java
@@ -25,6 +25,24 @@ public class NumArray {
     }
 }
 
+public class NumArray2 {
+    private int[] sums;
+    public NumArray(int[] nums) {
+        if (nums.length != 0){                      // 不然sum[0] = nums[0]那一步会报错
+            sums = new int[nums.length];
+        
+            sums[0] = nums[0];
+            for(int i=1; i<nums.length; i++){
+                sums[i] = nums[i] + sums[i-1];
+            }
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return i==0 ? sums[j] : sums[j]-sums[i-1];
+    }
+}
+
 // Your NumArray object will be instantiated and called as such:
 // NumArray numArray = new NumArray(nums);
 // numArray.sumRange(0, 1);
