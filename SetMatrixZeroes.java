c825c69273416289ac49badd7eadc314833ee407
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-07-07 21:05:32 +0800

    Update SetMatrixZeroes.java

--- SetMatrixZeroes.java
+++ SetMatrixZeroes.java
@@ -2,47 +2,42 @@
 Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
 */
 
-public class Solution {
-    public void setZeroes(int[][] matrix) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-        boolean flagrow = false, flagcol = false;
-        
-        for (int i = 0; i < col; i++) {
-            for (int j = 0; j < row; j++) {
-                if (matrix[j][i] == 0) {
-                    if (i == 0)
-                        flagcol = true;
-                    if (j == 0)
-                        flagrow = true;
-                    matrix[0][i] = 0;
-                    matrix[j][0] = 0;
-                }
-            }
-        }
-        
-        for (int i = 1; i < col; i++) {
-            if (matrix[0][i] == 0) {
-                for (int j = 0; j < row; j++)
-                    matrix[j][i] = 0;
-            }
-        }
-        
-        for (int j = 1; j < row; j++) {
-            if (matrix[j][0] == 0) {
-                for (int i = 0; i < col; i++)
-                    matrix[j][i] = 0;
-            }
-        }
-        
-        if (flagrow) {
-            for (int i = 0; i < col; i++) 
-                matrix[0][i] = 0;
-        }
-        
-        if (flagcol) {
-            for (int j = 0; j < row; j++)
-                matrix[j][0] = 0;
-        }
-    }
+public class SetMatrixZeroes {
+	public void setZeroes(int[][] matrix) {
+		int m = matrix.length;
+		int n = matrix[0].length;
+		boolean flagM = false, flagN = false;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == 0) {
+					if (i == 0)
+						flagN = true;
+					if (j == 0)
+						flagM = true;
+					matrix[0][j] = 0;
+					matrix[i][0] = 0;
+				}
+			}
+		}
+		for (int i = 1; i < m; i++) {
+			if (matrix[i][0] == 0) {
+				for (int j = 0; j < n; j++)
+					matrix[i][j] = 0;
+			}
+		}
+		for (int j = 1; j < n; j++) {
+			if (matrix[0][j] == 0) {
+				for (int i = 0; i < m; i++)
+					matrix[i][j] = 0;
+			}
+		}
+		if (flagM) {
+			for (int i = 0; i < m; i++)
+				matrix[i][0] = 0;
+		}
+		if (flagN) {
+			for (int j = 0; j < n; j++)
+				matrix[0][j] = 0;
+		}
+	}
 }
