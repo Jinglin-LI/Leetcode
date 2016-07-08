c928b50398125acb32b15f60e4ffc85feba89f8b
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-07-08 11:23:31 +0800

    Update RemoveDuplicatesFromSortedArrayII.java

--- RemoveDuplicatesFromSortedArrayII.java
+++ RemoveDuplicatesFromSortedArrayII.java
@@ -6,24 +6,34 @@ Given sorted array nums = [1,1,1,2,2,3],
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 */
 
-public class RemoveDuplicatesFromSortedArray {
+public class RemoveDuplicatesFromSortedArrayII {
 	public static void main(String[] args) {
-		int[] nums = {1,1,1,2,3};
-		System.out.println(new RemoveDuplicatesFromSortedArray().removeDuplicates(nums));
+		int[] nums = {1,1,1, 2,2,2,3};
+		System.out.println(new RemoveDuplicatesFromSortedArrayII().removeDuplicates(nums));
+		System.out.println(nums[0] + ", " + nums[1] + ", " + nums[2] + ", " + nums[3] + ", " + nums[4]+ ", " + nums[5]);
 	}
 	public int removeDuplicates(int[] nums) {
-		if (nums == null || nums.length == 0)
-			return 0;
-		if (nums.length == 1)
-			return 1;
-		int count = 1;
-		
+		if (nums == null)
+			return -1;
+		if (nums.length <= 2)
+			return nums.length;
+		int count = 1, size = 1;
 		for (int i = 1; i < nums.length; i++) {
 			if (nums[i] != nums[count - 1]) {
+				size = 1;
 				nums[count] = nums[i];
 				count++;
+			}
+			else if (nums[i] == nums[count - 1] && size == 1) {
+				nums[count] = nums[i];
+				count++;
+				size++;
+			}
+			else if (nums[i] == nums[count - 1] && size == 2) {
+				continue;
 			}
 		}
 		return count;
 	}
 }
+
