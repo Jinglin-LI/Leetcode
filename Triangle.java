1ac2f7190c9d92a4a853fb13fb7aa92a563fe391
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-07-12 21:07:21 +0800

    DP

--- Triangle.java
+++ Triangle.java
@@ -30,4 +30,16 @@ public class Solution {
         }
         return dp[0];
     }
+    
+    public int minimumTotal2(List<List<Integer>> triangle) {
+		if (triangle == null)
+			return 0;
+		for (int i = triangle.size() - 2; i >= 0; i--) {
+			for (int j = 0; j < triangle.get(i).size(); j++) {
+				triangle.get(i).set(j, Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)) + triangle.get(i).get(j));
+			}
+			
+		}
+		return triangle.get(0).get(0);
+	}
 }
