3a760e94adf34ef4699369d10abded141dc3b877
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-09-06 05:12:26 +0800

    Topological sort

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -10,6 +10,10 @@ There are a total of 2 courses to take. To take course 1 you should have finishe
 Note:
 The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
 */
+// For the Topological sort method, reference: http://blog.csdn.net/ljiabin/article/details/45846837
+// 构建图，[[],[],[],[]], index为先修课的名称（0,1,2）. 从“入度”为0的节点加入到Q中，把这个节点（先修课）后面的课程的入度减去1.
+// 用count记录numCourses, 用以“删除”节点。
+
 
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
@@ -35,4 +39,30 @@ public class Solution {
         status[index] = 2;
         return true;
     }
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
+		List<List<Integer>> posts = new ArrayList<>();
+		for (int i = 0; i < numCourses; i++) {
+			posts.add(new ArrayList<>());
+		}
+		int[] preNums = new int[numCourses];
+		for (int i = 0; i < prerequisites.length; i++) {
+			posts.get(prerequisites[i][1]).add(prerequisites[i][0]);
+			preNums[prerequisites[i][1]]++;
+		}
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 0; i < numCourses; i++) {
+			if (preNums[i] == 0)
+				q.add(i);
+		}
+		int count = numCourses;
+		while (!q.isEmpty()) {
+			int x = q.poll();
+			for (int i : posts.get(x)) {
+				if (--preNums[i] == 0)
+					q.add(i);
+			}
+			count--;
+		}
+		return count == 0;
+	}
 }
