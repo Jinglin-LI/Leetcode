a80b12ed08d118901c5cbd12f7ba2184f9964eac
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-09-18 03:31:00 +0800

    Hard, String

--- LongestValidParentheses.java
+++ LongestValidParentheses.java
@@ -3,7 +3,9 @@ Given a string containing just the characters '(' and ')', find the length of th
 For "(()", the longest valid parentheses substring is "()", which has length = 2.
 Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
 */
-// 用stack，存储'('的index，当i指向')'时，pop出相应的一个'('， 如果stack不为空，接着用i-stack.peek(). 如果stack为空，i与start相减，重新设定start。
+// 用stack，存储'('的index，当i指向')'时，如果stack空，“())”时，start重新设定为2. 如不为空： 
+// 1）pop出相应的一个'('， 如果stack不为空，start接着用，用i-stack.peek()表示目前最长的。
+// 2）如果stack为空，i与start相减。
 
 public class Solution {
     public int longestValidParentheses(String s) {
