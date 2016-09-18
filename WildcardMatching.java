1afc2aba2e0bab2515e9765d2461887360eb1c0b
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-09-19 01:29:17 +0800

    Hard, String

--- WildcardMatching.java
+++ WildcardMatching.java
@@ -16,8 +16,13 @@ isMatch("aa", "a*") → true
 isMatch("ab", "?*") → true
 isMatch("aab", "c*a*b") → false
 */
-// 两个字符串各两个指针，如果p中'*'存在，flag为真，记录当前两个指针的位置为preS与preP. indexS继续移动（都与'*'相配）。
+// 贪心算法。两个字符串各两个指针，如果p中'*'存在，flag为真，记录当前两个指针的位置为preS与preP. indexS继续移动（都与'*'相配）。
 // 当之后遇到不相同时，preS++，indexS回溯成preS， preP不变， indexP回溯成preP. 例子：abcdade, ab*de.
+/*
+update: 增加了DP的解法(LC会超时)：Matrix了后，注意dp[][]的用法。dp[i][j] 与 s.charAt(i - 1)相比
+1. 初始化：dp[0][0] = true, 因为空与空相配为true. ‘*’与空相配也为true。所以dp[0][j] = dp[0][j - 1] && p.charAt(i - 1) == '*';
+2. 遍历：‘*’可以与0,1,2...个元素配对。即， dp[i][j] = dp[i - 1][j - 1], or = dp[i - 1][j] or = dp[i][j - 1].  
+*/
 
 public class Solution {
     public boolean isMatch(String s, String p) {
@@ -49,7 +54,6 @@ public class Solution {
             else
                 return false;
         }
-        
         while (indexP < p.length() && p.charAt(indexP) == '*')    // 忽略p中末尾的*. 因为之前判断indexS < s.length(), 最后判断都到末尾
             indexP++;
         
@@ -60,4 +64,24 @@ public class Solution {
     private boolean isMatch(char a, char b) {
         return (b == '?' || a == b);
     }
+    
+    // add the DP solution
+    public boolean isMatch2(String s, String p) {
+		int m = s.length();
+		int n = p.length();
+		boolean[][] dp = new boolean[m + 1][n + 1];
+		dp[0][0] = true;                                                        // 注意dp[0][0]的用法。dp[i][j] 与 s.charAt(i - 1)相比
+		for (int i = 1; i <= n; i++) {
+			dp[0][i] = dp[0][i - 1] && p.charAt(i - 1) == '*';                  // 初始化
+		}
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (isMatch(s.charAt(i - 1), p.charAt(j - 1)))
+					dp[i][j] = dp[i - 1][j - 1];
+				else if (p.charAt(j - 1) == '*')
+					dp[i][j] = dp[i - 1][j - 1] || dp[i][j - 1] || dp[i - 1][j];
+			}
+		}
+		return dp[m][n];
+	}
 }
