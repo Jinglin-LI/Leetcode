b3834530f2a4c0115441f020b86242704f7c39f9
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-09-20 21:44:00 +0800

    Hard, String

--- PalindromePartitioningII.java
+++ PalindromePartitioningII.java
@@ -6,9 +6,12 @@ Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 
 */
 // 找最少的切割数。
 /*
+http://www.cnblogs.com/springfor/p/3891896.html
 两个指针 + DP。 
-dp[i]代表string的i到len的字符串的最小切割数。所以从i开始到末尾以j (i < j < n)为划分点的最小划分数为: dp[j+1]+1 和 dp[i]中的最小值。
-cut_num[i]的初值设为：s.length() - i; 也就是按照字符串中的每个字母都单独被划分来计算。
+首先设置dp变量 dp[len+1]。dp[i]表示从第i位置到第len位置（包含，即[i, len])的切割数（第len位置为空）。
+初始时，是len-i。比如给的例子aab，cuts[0]=3，就是最坏情况每一个字符都得切割：a|a|b|' '。cuts[1] = 2, 即从i=1位置开始，a|b|' '。
+dp[2] = 1 b|' '。dp[3]=0,即第len位置，为空字符，不需要切割
+所以从i开始到末尾以j (i < j < n)为划分点的最小划分数为: dp[j+1]+1 和 dp[i]中的最小值。
 判断回文，每一个字符判断太花时间。则用二维数组palindrome[][] 为1代表。判断palindrome[i][j]则palindrome[i + 1][j - 1]一定为1. 
 */
 
@@ -25,7 +28,7 @@ public class PalindromePartitioningII {
 		for (int i = len - 1; i >= 0; i--) {
 			for (int j = i; j < len; j++) {
 				if (s.charAt(i) == s.charAt(j)) {
-					if (j - i < 2 || palindrome[i + 1][j - 1] == true) {           // j与i为相同，或者只有一个字符（j - i == 1）.
+					if (j - i < 2 || palindrome[i + 1][j - 1] == true) {     // j与i为相同，或者只有一个字符（j - i == 1）.
 						palindrome[i][j] = true;
 						dp[i] = Math.min(dp[i], dp[j + 1] + 1);
 					}
