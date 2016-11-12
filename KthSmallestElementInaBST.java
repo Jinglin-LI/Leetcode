639b872b95b90778e3d229fe77577e7d2ababe52
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-13 05:55:03 +0700

    Update KthSmallestElementInaBST.java

--- KthSmallestElementInaBST.java
+++ KthSmallestElementInaBST.java
@@ -40,3 +40,24 @@ public class Solution {
         return 0;
     }
 }
+
+/*===========================================================================================================*/
+
+// recursive
+ // 左子数数一下个数，看与k比较的大小。
+ 
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k-1-count);
+        }
+        return root.val;
+    }
+    public int countNodes(TreeNode n) {
+        if (n == null) return 0;
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+}
