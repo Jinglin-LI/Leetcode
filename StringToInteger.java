599b534a8d78c8ed5fac2cbd180a938874f7fff5
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-13 01:35:56 +0700

    Update StringToInteger.java

--- StringToInteger.java
+++ StringToInteger.java
@@ -10,6 +10,49 @@ If the first sequence of non-whitespace characters in str is not a valid integra
 If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
 */
 
+
+
+// 用double值存储res. 最后再判断是否大于MAX_VALUE
+
+public class Solution {
+    public int myAtoi(String str) {
+        if(str == null || str.length() == 0){
+            return 0;
+        }
+        str = str.trim();
+        int flag = 1;
+        double result = 0;
+        int i = 0;
+        if(str.charAt(0) == '-'){
+            flag = -1;
+            i++;
+        }
+        else if(str.charAt(0) == '+'){
+            i++;
+        }
+        while(i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9'){
+            result = result * 10 + (str.charAt(i) - '0');
+            i++;
+        }
+        if(flag == -1){
+            result = -result;
+        }
+        if(result > Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }
+        if(result < Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+        }
+        return (int)result;
+    }
+}
+
+
+
+/*----------------------------------------------------------------------------------------*/
+
+// 此方法要比较最后一位是否大于Max value
+
 public class Solution {
     public int myAtoi(String str) {
         int sign = 1;
