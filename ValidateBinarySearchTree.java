575cefb64b4142c0cc2aad975d7ebfb7537572e1
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-12 23:53:06 +0700

    Update ValidateBinarySearchTree.java

--- ValidateBinarySearchTree.java
+++ ValidateBinarySearchTree.java
@@ -16,6 +16,27 @@ Assume a BST is defined as follows:
  * }
  */
  
+
+
+
+
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    private boolean isValid(TreeNode root, long min, long max){
+        if(root == null){
+            return true;
+        }
+        if(root.val <= min || root.val >= max){
+            return false;
+        }
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+    }
+
+
+/*========================================================================================================*/
+
+
 public class Solution {
     public boolean isValidBST(TreeNode root) {
         if (root == null)
