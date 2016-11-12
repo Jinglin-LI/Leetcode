90af90849e51a87e2993ba5e771b28bc4592059e
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-13 01:30:07 +0700

    Update MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -4,6 +4,20 @@ For example,
 Given nums = [0, 1, 3] return 2.
 */
 
+
+    public int missingNumber(int[] nums) {
+        int xor = 0, i; 
+ 
+        //用异或，将index和value都同时异或，成对的index和value在异或以后就为0，也就是相互抵消了，剩下的再与for循环后的i异或就是结果
+        for(i = 0; i < nums.length; i++){ 
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ i;
+    }
+
+/*======================================================================================================================*/
+
+
 public class Solution {
     public int missingNumber(int[] nums) {
         int len = nums.length;
