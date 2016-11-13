cacd9a684143646d21bffc4a0158e7bf9df3ef87
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-14 00:36:07 +0700

    Update FirstBadVersion.java

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -24,3 +24,28 @@ public class Solution extends VersionControl {
         return 0;
     }
 }
+
+
+
+/*=========================================================================================================*/
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n < 1){
+            return 0;
+        }
+        int left = 1;
+        int right = n;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(!isBadVersion(mid)){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+
