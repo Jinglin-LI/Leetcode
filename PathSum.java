7774325864b3d21cb5276ee9ce3692abb96c6fdf
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-14 00:54:14 +0700

    Update PathSum.java

--- PathSum.java
+++ PathSum.java
@@ -21,7 +21,25 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
  *     TreeNode(int x) { val = x; }
  * }
  **/
- 
+
+ // recursive
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null){
+            return false;
+        }
+        if(root.val == sum && (root.left == null && root.right == null)){
+            return true;
+        }
+        return (hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val));
+    } 
+}
+
+/*=======================================================================================================*/
+
+
+// not recursive, using stack
+
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null)
