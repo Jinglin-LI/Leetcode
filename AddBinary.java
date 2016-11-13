204ebe83c7c60b608355d85dae33e9ba27defabc
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-13 08:58:58 +0700

    Update AddBinary.java

--- AddBinary.java
+++ AddBinary.java
@@ -7,6 +7,24 @@ b = "1"
 Return "100".
 */
 
+public class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if(carry == 1) sb.append(1);
+        return sb.reverse().toString();
+    }
+}
+
+/*========================================================================================================*/
+
 public class AddBinary {
 	public static void main(String[] args) {
 		System.out.println(new AddBinary().addBinary("1", "111"));
