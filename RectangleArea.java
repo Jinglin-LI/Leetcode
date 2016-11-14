77efa71864ef935bdd0f4ec6d531ce8cd7fa4fb3
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-14 23:27:13 +0700

    Update RectangleArea.java

--- RectangleArea.java
+++ RectangleArea.java
@@ -1,3 +1,5 @@
+//         c,d                  g,h
+//     a,b                  e,f
 /*
 Find the total area covered by two rectilinear rectangles in a 2D plane.
 Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
