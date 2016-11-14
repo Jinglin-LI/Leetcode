f6257b9ca37be226c0be11cd1b4e8117d13aa299
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-14 07:01:38 +0700

    Update TrappingRainWater.java

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -36,3 +36,30 @@ public class Solution {
         return res;
     }
 }
+
+
+/*=======================================================================================================*/
+
+
+
+public class Solution {
+    public int trap(int[] height) {
+        int secheight = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int s = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                secheight = Math.max(secheight, height[left]);
+                s += secheight - height[left];
+                left++;
+            }
+            else{
+                secheight = Math.max(secheight, height[right]);
+                s += secheight - height[right];
+                right--;
+            }
+        }
+        return s;
+    }
+}
