17a27cee488547c9128badb99c19f04135c32c13
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-11-15 00:00:16 +0700

    Update LongestPalindrome.java

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,3 +1,4 @@
+/*
 Given a string which consists of lowercase or uppercase letters, 
 find the length of the longest palindromes that can be built with those letters.
 This is case sensitive, for example "Aa" is not considered a palindrome here.
@@ -10,6 +11,7 @@ Output:
 7
 Explanation:
 One longest palindrome that can be built is "dccaccd", whose length is 7.
+*/
 
 import java.util.Arrays;
 
