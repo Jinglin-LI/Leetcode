9a93d37984dca1b6f6c66fd026cdc000a37952dd
静林-李 <lijinglin@mail.nankai.edu.cn>
2016-12-23 04:38:49 +0700

    Update SerializeAndDeserializeBinaryTree.java

--- SerializeAndDeserializeBinaryTree.java
+++ SerializeAndDeserializeBinaryTree.java
@@ -20,70 +20,111 @@ For example, you may serialize the following tree
 as "[1,2,3,null,null,4,5]", just the same as how LeetCode OJ serializes a binary tree. 
 You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
 */
+// Reference: http://www.programcreek.com/2014/05/leetcode-serialize-and-deserialize-binary-tree-java/
 
-// BFS, Order Level
 
-public class Codec {
-
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        StringBuilder sb = new StringBuilder();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode temp = q.poll();
-            if (temp == null) {
-                sb.append("#,");
-            }
-            else {
-                sb.append(temp.val + ",");
-                q.add(temp.left);
-                q.add(temp.right);
-            }
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
-    }
-
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        if (data == null || data.length() == 0 || data.charAt(0) == '#')
-            return null;
-        String[] arr = data.split(",");
-        TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        int i = 1;
-        while (!q.isEmpty()) {
-            TreeNode temp = q.poll();
-            if (temp == null) {
-                continue;
-            }
-            if (!arr[i].equals("#")) {
-                temp.left = new TreeNode(Integer.parseInt(arr[i]));
-                q.add(temp.left);
-            }
-            else {
-                temp.left = null;
-                q.add(null);
-            }
-            i++;
-            
-            if (!arr[i].equals("#")) {
-                temp.right = new TreeNode(Integer.parseInt(arr[i]));
-                q.add(temp.right);
-            }
-            else {
-                temp.right = null;
-                q.add(null);
-            }
-            i++;
-        }
-        return root;
-    }
+import java.util.*;
+public class SerializeAndDeserializeBinaryTree {
+    // BFS, Order Level Traversal
+	public String serialize(TreeNode root) {
+		StringBuilder sb = new StringBuilder();
+		Queue<TreeNode> q = new LinkedList<>();
+		q.add(root);
+		
+		while (!q.isEmpty()) {
+			TreeNode temp = q.poll();
+			if (temp == null) {
+				sb.append("#,");
+			}
+			else {
+				sb.append(temp.val + ",");
+				q.add(temp.left);
+				q.add(temp.right);
+			}
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+	
+	public TreeNode deserialize(String data) {
+		if (data == null || data.length() == 0)
+			return null;
+		String[] arr = data.split(",");
+		TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
+		Queue<TreeNode> q = new LinkedList<>();
+		q.add(root);
+		
+		int i = 1;
+		while (!q.isEmpty()) {
+			TreeNode temp = q.poll();
+			if (temp == null) {
+				continue;
+			}
+			if (arr[i] != "#") {
+				temp.left = new TreeNode(Integer.parseInt(arr[i]));
+				q.add(temp.left);
+			}
+			else {
+				temp.left = null;
+				q.add(null);
+			}
+			i++;
+			
+			if (arr[i] != "#") {
+				temp.right = new TreeNode(Integer.parseInt(arr[i]));
+				q.add(temp.right);
+			}
+			else {
+				temp.right = null;
+				q.add(null);
+			}
+			i++;
+		}
+		return root;
+	}
+	
+/*====== Preorder， recursive 递归 ================================*/
+	
+	public String serialize2(TreeNode root) {
+		if (root == null)
+			return null;
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+		StringBuilder sb = new StringBuilder();
+		while (!stack.isEmpty()) {
+			TreeNode temp = stack.pop();
+			if (temp != null) {
+				sb.append(temp.val + ",");
+				stack.push(temp.right);
+				stack.push(temp.left);
+			}
+			else {
+				sb.append("#,");
+			}
+		}
+		return sb.toString().substring(0, sb.length() - 1);
+	}
+	public TreeNode deserialize2(String data) {
+		if (data == null)
+			return null;
+		String[] arr = data.split(",");
+		
+		// 相当于指针，传进来了，地址会改变。
+		int[] root = {0};
+		return helper(arr, root);
+	}
+	public TreeNode helper(String[] arr, int[] node) {
+		if (arr[node[0]].equals("#")) {
+			return null;
+		}
+		TreeNode root = new TreeNode(Integer.parseInt(arr[node[0]]));
+		
+		node[0]++;
+		root.left = helper(arr, node);
+		
+		node[0]++;
+		root.right = helper(arr, node);
+		
+		return root;
+	}
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.deserialize(codec.serialize(root));
