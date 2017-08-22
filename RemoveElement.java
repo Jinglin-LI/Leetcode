c120197a88fd951b9cc599b40dabb26883cffc22
静林-李 <lijinglin@mail.nankai.edu.cn>
2017-08-22 22:56:49 +0700

    Update RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -11,8 +11,7 @@ public class RemoveElement {
 	public int removeElement(int[] nums, int val) {
 		if (nums == null || nums.length == 0)
 			return 0;
-		if (nums.length == 1)
-			return nums[0] == val? 0 : 1;
+
 		int count = 0;
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] != val)
