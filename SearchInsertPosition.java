27d72ccf3d269b337a7e83b9169eae2b3d171609
静林-李 <lijinglin@mail.nankai.edu.cn>
2017-08-23 00:37:49 +0700

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -9,6 +9,27 @@ Here are few examples.
 */
 
 public class Solution {
+    
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
+        int low = 0; 
+        int high = nums.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+    
+    /**************************************************************************************************/
+    
+    // discard this
     public int searchInsert(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
