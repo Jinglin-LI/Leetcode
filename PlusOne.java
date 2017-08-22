b2f153c9aed09336322498ce8ec4b40030811fc1
静林-李 <lijinglin@mail.nankai.edu.cn>
2017-08-23 04:45:39 +0700

    Update PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -4,6 +4,26 @@ The digits are stored such that the most significant digit is at the head of the
 */
 
 public class Solution {
+	public int[] plusOne(int[] digits) {
+        int i = digits.length - 1;
+        
+        while (i >= 0 && digits[i] == 9) {
+            digits[i--] = 0;
+        }
+        if (i == -1) {
+            int[] res = new int[digits.length + 1];
+            Arrays.fill(res, 0);
+            res[0] = 1;
+            return res;
+        }
+        else {
+            digits[i] += 1;
+        }
+        return digits;  
+}
+	
+/***************************************************************************************/
+	
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
