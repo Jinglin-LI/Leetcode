4a73a452245516fc1da32000314d1b8b13c68582
静林-李 <lijinglin@mail.nankai.edu.cn>
2017-08-23 09:15:25 +0700

    Update ContainsDuplicateII.java

--- ContainsDuplicateII.java
+++ ContainsDuplicateII.java
@@ -3,6 +3,22 @@ Given an array of integers and an integer k, find out whether there are two dist
 */
 
 public class Solution {
+    
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> hs = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                hs.remove(nums[i - k - 1]);             // hashSet.remove
+            }
+            if (!hs.add(nums[i])) {                     // add to hashSet if there is no duplicates. !hs.add() 意味着存在duplicate
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /********************************************************************/
+    
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> hm = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
