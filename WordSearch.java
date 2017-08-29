0b29c7ee7710732feb2c492417c47f0ec10acc1d
静林-李 <lijinglin@mail.nankai.edu.cn>
2017-08-30 00:13:19 +0700

    Update WordSearch.java

--- WordSearch.java
+++ WordSearch.java
@@ -14,6 +14,51 @@ word = "ABCB", -> returns false.
 */
 
 public class Solution {
+  
+  // 可按照传统visited传入到DFS
+    public boolean exist(char[][] board, String word) {
+        if (board == null || word == null)
+            return false;
+        if (word.length() == 0)
+            return true;
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, i, j, 0, word, visited)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, int i, int j, int index, String word, boolean[][] visited) {
+        if (index == word.length()) {
+            return true;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word.charAt(index) || visited[i][j]) {
+            return false;
+        }
+        
+        visited[i][j] = true;
+        boolean res = dfs(board, i - 1, j, index + 1, word, visited) ||
+            dfs(board, i + 1, j, index + 1, word, visited) ||
+            dfs(board, i, j + 1, index + 1, word, visited) ||
+            dfs(board, i, j - 1, index + 1, word, visited);
+        visited[i][j] = false;
+        return res;
+    }
+  
+  /**************************************************************************************/
+  
+  // 一样的方法。可以用‘~’标记visited，然后再改回来。
     public boolean exist(char[][] board, String word) {
         if (board == null || word == null)
             return false;
