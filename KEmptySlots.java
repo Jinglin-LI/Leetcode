4f357ace686b4e4380a9adb51215aef31d6add43
静林-李 <lijinglin@mail.nankai.edu.cn>
2017-11-14 18:54:26 -0600

    Update KEmptySlots.java

--- KEmptySlots.java
+++ KEmptySlots.java
@@ -12,6 +12,20 @@ Also given an integer k, you need to output in which day there exists two flower
 and also the number of flowers between them is k and these flowers are not blooming.
 
 If there isn't such day, output -1.
+
+Example1:
+Input:
+flowers:[1,3,2]
+k:1
+Output:2
+Explanation: In the second day, the first and the third flower have been blooming. 
+
+Example2: 
+Input:
+flowers:[1,2,3]
+k:1
+Output:-1
+
 */
 
 import java.util.HashSet;
