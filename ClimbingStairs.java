28e0038823cec5e8b2dd5622ada8173183bb58a0
静林-李 <lijinglin@mail.nankai.edu.cn>
2017-11-28 15:09:52 -0600

    Update ClimbingStairs.java

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -3,6 +3,23 @@ You are climbing a stair case. It takes n steps to reach to the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 */
 
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+        
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+}
+
+/************************************************************************************************/
+
 public class Solution {
     public int climbStairs(int n) {
         int p = 1;
@@ -17,3 +34,5 @@ public class Solution {
         return q;
     }
 }
+
+
