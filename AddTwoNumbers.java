a9b561a5f7180015a33c8e3296bbfd269cd37527
静林-李 <lijinglin@mail.nankai.edu.cn>
2020-08-02 08:59:43 -0500

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -20,6 +20,30 @@ Output: 7 -> 0 -> 8
  
 import java.util.*;
 public class AddTwoNumbers {
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = dummyHead;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int i = (l1 != null)? l1.val : 0;
+            int j = (l2 != null)? l2.val : 0;
+            int total = i + j + + carry;
+            p.next = new ListNode(total % 10);
+            carry = total / 10;
+            p = p.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            p.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+/////////////////////////////////////////////////////////////////	
 	public static void main(String[] args) {
 		
 	}
